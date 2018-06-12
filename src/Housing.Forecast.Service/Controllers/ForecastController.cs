8e86e4b76f5619c0cb3f855bf00c65ab1ccde12c
Manjinder Singh <manjinder360@yahoo.com>
2018-06-12 10:11:48 -0400

    Started setting up the forecast controller.

--- src/Housing.Forecast.Service/Controllers/ForecastController.cs
+++ src/Housing.Forecast.Service/Controllers/ForecastController.cs
@@ -3,59 +3,62 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
+using Housing.Forecast.Context;
+using Housing.Forecast.Context.Repos;
+using System.Collections.Generic;
 
 namespace Housing.Forecast.Service.Controllers
 {
-  [Route("api/[controller]")]
-  public class ForecastController : BaseController
-  {
-    public ForecastController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton)
-      : base(loggerFactory, queueClientSingleton) {}
-    
-    public async Task<IActionResult> Get()
+    [Route("api/[controller]")]
+    public class ForecastController : BaseController
     {
-      return await Task.Run(() => Ok());
-    }
+        private readonly SnapshotRepo _snapshot;
+        public ForecastController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton)
+          : base(loggerFactory, queueClientSingleton) { }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> Get(int id)
-    {
-      return await Task.Run(() => Ok());
-    }
+        /// <summary>
+        /// This endpoint will return all unique locations of rooms
+        /// </summary>
+        /// <return>
+        /// Return a list of all unique locations of rooms.
+        /// </return>
+        // GET: api/forecast/Locations
+        [HttpGet("Locations")]
+        public IActionResult GetLocations()
+        {
+            try
+            {
 
-    [HttpPost]
-    public async Task<IActionResult> Post([FromBody]object value)
-    {
-      return await Task.Run(() => Ok());
-    }
+                List<string> locations = _snapshot.GetLocations();
+                if (locations.Count == 0)
+                {
+                    return NotFound("No locations found.");
+                }
+                return Ok(locations);
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, [FromBody]object value)
-    {
-      return await Task.Run(() => Ok());
-    }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                return BadRequest("Something went wrong while processing the request.");
+            }
+        }
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
-    {
-      return await Task.Run(() => Ok());
-    }
+        protected override void UseReceiver()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ReceiverExceptionHandler)
+            {
+                AutoComplete = false
+            };
 
-    protected override void UseReceiver()
-    {
-      var messageHandlerOptions = new MessageHandlerOptions(ReceiverExceptionHandler)
-      {
-        AutoComplete = false
-      };
+            queueClient.RegisterMessageHandler(ReceiverMessageProcessAsync, messageHandlerOptions);
+        }
 
-      queueClient.RegisterMessageHandler(ReceiverMessageProcessAsync, messageHandlerOptions);
+        protected override void UseSender(Message message)
+        {
+            Task.Run(() =>
+              SenderMessageProcessAsync(message)
+            );
+        }
     }
-    
-    protected override void UseSender(Message message)
-    {
-      Task.Run(() =>
-        SenderMessageProcessAsync(message)
-      );
-    }
-  }
 }
