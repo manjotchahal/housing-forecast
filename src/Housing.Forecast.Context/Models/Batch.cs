c2a38bb65bb3dbb7fd6b8ca97bf1baf71fb45988
pjrivera08 <pj.rivera08@gmail.com>
2018-06-13 09:24:01 -0400

    Added Batch model to Context [#158038842]

---
+++ src/Housing.Forecast.Context/Models/Batch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Housing.Forecast.Context.Models
+{
+    public class Batch
+    {
+        /// <summary>Default Constructor</summary>>
+        /// <remarks>Sets all properties to empty, null, or impossible values that correspond 
+        /// to invalid models that should be invalid if not replaced.</remarks>
+        public Batch()
+        {
+            Id = Guid.Empty;
+            BatchId = Guid.Empty;
+            BatchName = "";
+            BatchOccupancy = -1;
+            BatchSkill = "";
+            Address = null;
+        }
+
+        /// <summary>Property validation</summary>>
+        /// <remarks>Returns true if all properties are valid
+        /// By default (through constructor), the model is invalid, properties to be filled in</remarks>
+        public bool Validate()
+        {
+            if (Id == Guid.Empty) { return false; }
+            if (BatchId == Guid.Empty) { return false; }
+            if (String.IsNullOrEmpty(BatchName)) { return false; }
+            if (BatchOccupancy < 0 || BatchOccupancy > 100) { return false; }
+            if (String.IsNullOrEmpty(BatchSkill)) { return false; }
+            if (Address == null) { return false; }
+
+            return true;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public Guid BatchId { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime StartDate { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime EndDate { get; set; }
+
+        public string BatchName { get; set; }
+
+        public int BatchOccupancy { get; set; }
+
+        public string BatchSkill { get; set; }
+
+        public Address Address { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime Created { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime Deleted { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+    }
+}
