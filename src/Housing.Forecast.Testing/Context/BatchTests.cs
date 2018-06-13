4b770df7a8e9abf89f271db61d3373ff735cf972
pjrivera08 <pj.rivera08@gmail.com>
2018-06-13 09:58:25 -0400

    Added tests for Context models. [#158038842]

---
+++ src/Housing.Forecast.Testing/Context/BatchTests.cs
@@ -0,0 +1,105 @@
+﻿using Housing.Forecast.Context.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Housing.Forecast.Testing.Context
+{
+    public class BatchTests
+    {
+        private Batch getTestBatch()
+        {
+            Batch batch = new Batch
+            {
+                Id = Guid.NewGuid(),
+                BatchId = Guid.NewGuid(),
+                BatchName = "name",
+                BatchOccupancy = 1,
+                BatchSkill = ".Net",
+                Address = new Address
+                {
+                    Id = Guid.NewGuid(),
+                    AddressId = Guid.NewGuid(),
+                    Address1 = "1600 Pennsylvania Ave",
+                    Address2 = "Apt. 110-B",
+                    City = "Reston",
+                    State = "VA",
+                    PostalCode = "12345-1234",
+                    Country = "USA",
+                    Created = DateTime.Now
+                }
+            };
+            return batch;
+        }
+        [Fact]
+        public void DefaultBatchInvalidTest()
+        {
+            Batch test = new Batch();
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchValidTest()
+        {
+            Batch test = getTestBatch();
+            Assert.True(test.Validate());
+        }
+
+        [Fact]
+        public void BatchIdInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.Id = Guid.Empty;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchBatchIdInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.BatchId = Guid.Empty;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchNameInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.BatchName = "";
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchOccupancyLessInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.BatchOccupancy = -1;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchOccupancyOverInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.BatchOccupancy = 101;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchSkillInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.BatchSkill = "";
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void BatchAddressInvalidTest()
+        {
+            Batch test = getTestBatch();
+            test.Address = null;
+            Assert.False(test.Validate());
+        }
+    }
+}
