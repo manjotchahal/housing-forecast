9b06bbdf7b8368b47a5d62cbd556559d0e87a70d
pjrivera08 <pj.rivera08@gmail.com>
2018-06-13 09:35:30 -0400

    Removed data annotations and added documentation. [#158038842]

--- src/Housing.Forecast.Library/Models/Snapshot.cs
+++ src/Housing.Forecast.Library/Models/Snapshot.cs
@@ -9,22 +9,16 @@ namespace Housing.Forecast.Library.Models
     ///<summary>The Snapshot class is used to represent the supply and demand of Rooms and Users on any given date.</summary>
     public class Snapshot
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+       
         public Guid Id { get; set; }
-
-        [Column(TypeName = "datetime2")]
         public DateTime Date { get; set; }
-
         public int RoomCount { get; set; }
-
         public int UserCount { get; set; }
-
         public string Location { get; set; }
-
-        [Column(TypeName = "datetime2")]
         public DateTime Created { get; set; }
-
+        /// <summary>Default Constructor</summary>>
+        /// <remarks>Sets all properties to empty, null, or impossible values that correspond 
+        /// to invalid models that should be invalid if not replaced.</remarks>
         public Snapshot() {
             Id = Guid.Empty;
             Date = DateTime.MinValue;
@@ -32,7 +26,9 @@ namespace Housing.Forecast.Library.Models
             RoomCount = -1;
             UserCount = -1;
         }
-
+        /// <summary>Property validation</summary>>
+        /// <remarks>Returns true if all properties are valid
+        /// By default (through constructor), the model is invalid, properties to be filled in</remarks>
         public bool Validate() {
             bool result = Id != Guid.Empty &&
                 Date > DateTime.MinValue &&
