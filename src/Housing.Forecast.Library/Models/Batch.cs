87f07ffd31ba5c43bbf5d8892c2a3bff6606196f
Manjinder Singh <manjinder360@yahoo.com>
2018-06-13 09:18:01 -0400

    Merged with the latest changes.

--- src/Housing.Forecast.Library/Models/Batch.cs
+++ src/Housing.Forecast.Library/Models/Batch.cs
@@ -9,7 +9,6 @@ namespace Housing.Forecast.Library.Models
     ///<summary>The Batch model is used to contain all of the pertinent information about a batch including start date, end date, location, type, occupancy, and location. </summary>
     ///<remarks>Each Batch contains a collection of User objects that represents the associates that belong to a batch.
     ///Each Batch will have a uniquely generated Guid Id as well as retain the primary key Guid of the previous database, which is stored in BatchId.</remarks>
-    [Table("Batches")]
     public class Batch
     {
         /// <summary>Default Constructor</summary>>
@@ -33,47 +32,31 @@ namespace Housing.Forecast.Library.Models
             if (Id == Guid.Empty) { return false; }
             if (BatchId == Guid.Empty) { return false; }
             if (String.IsNullOrEmpty(BatchName)) { return false; }
-            if (BatchOccupancy == -1) { return false; }
+            if (BatchOccupancy < 0 ||  BatchOccupancy > 100) { return false; }
             if (String.IsNullOrEmpty(BatchSkill)) { return false; }
             if (Address == null) { return false; }
 
             return true;
         }
 
-
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [ScaffoldColumn(false)]
         public Guid Id { get; set; }
 
-        [Required]
         public Guid BatchId { get; set; }
 
-
-        [DataType(DataType.DateTime)]
         [Column(TypeName = "datetime2")]
         public DateTime StartDate { get; set; }
 
-        [DataType(DataType.DateTime)]
         [Column(TypeName = "datetime2")]
         public DateTime EndDate { get; set; }
 
-
-        [DataType(DataType.Text)]
-        [Required]
-        [Column(TypeName = "nvarchar(MAX)")]
         public string BatchName { get; set; }
 
-        [Range(0, 100)]
-        [Required]
         public int BatchOccupancy { get; set; }
 
-        [DataType(DataType.Text)]
-        [Required]
-        [Column(TypeName = "nvarchar(MAX)")]
         public string BatchSkill { get; set; }
 
-        [Required]
         public Address Address { get; set; }
 
         [Column(TypeName = "datetime2")]
--- src/Housing.Forecast.Library/Models/Room.cs
+++ src/Housing.Forecast.Library/Models/Room.cs
@@ -11,34 +11,22 @@ namespace Housing.Forecast.Library.Models
     ///Each Room object will have its own uniquely generated Guid Id and retain the primary key Guid that was generated for it in the previous database into RoomId.
     ///Each Room object will have a collection of User objects that share the same Room.
     ///</remarks>
-    [Table("Rooms")]
     public class Room
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
-        // Id we get from service hub
-        [Required]
         public Guid RoomId { get; set; }
 
-        [DataType(DataType.Text)]
-        [Column(TypeName = "nvarchar(MAX)")]
-        [Required]
         public string Location { get; set; }
 
-        [Required]
         public int Vacancy { get; set; }
 
-        [Required]
         public int Occupancy { get; set; }
 
-        [DataType(DataType.Text)]
-        [Column(TypeName = "nvarchar(MAX)")]
-        [Required]
         public string Gender { get; set; }
 
-        [Required]
         public Address Address { get; set; }
 
         [Column(TypeName = "datetime2")]
