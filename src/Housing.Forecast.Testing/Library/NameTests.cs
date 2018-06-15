33b4fa9b126f930ba0012a18d7afd8ece8245c1a
Manjinder Singh <manjinder360@yahoo.com>
2018-06-15 14:32:06 -0400

    Merged the latest changes.

---
+++ src/Housing.Forecast.Testing/Library/NameTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Xunit;
+using Housing.Forecast.Library.Models;
+using Housing.Forecast.Service;
+
+namespace Housing.Forecast.Testing.Library
+{
+    public class NameTests
+    {
+        private Name getTestName()
+        {
+            Name result = new Name
+            {
+                Id = Guid.NewGuid(),
+                NameId = Guid.NewGuid(),
+                First = "first",
+                Middle = "middle",
+                Last = "last",
+                Created = DateTime.Now,
+            };
+            return result;
+        }
+
+        [Fact]
+        public void DefaultNameInvalidTest()
+        {
+            Name test = new Name();
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void NameValidTest()
+        {
+            Name test = getTestName();
+            Assert.True(test.Validate());
+        }
+
+        [Fact]
+        public void NameIdInvalidTest()
+        {
+            Name test = new Name();
+            test.Id = Guid.Empty;
+            Assert.False(test.Validate());
+        }
+        [Fact]
+        public void NameNameIdInvalidTest()
+        {
+            Name test = new Name();
+            test.NameId = Guid.Empty;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void NameFirstNullInvalidTest()
+        {
+            Name test = new Name();
+            test.First = null;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void NameFirstEmptyInvalidTest()
+        {
+            Name test = new Name();
+            test.First = "";
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void NameLastNullInvalidTest()
+        {
+            Name test = new Name();
+            test.Last = null;
+            Assert.False(test.Validate());
+        }
+
+        [Fact]
+        public void NameLastEmptyInvalidTest()
+        {
+            Name test = new Name();
+            test.Last = "";
+            Assert.False(test.Validate());
+        }
+    }
+}
