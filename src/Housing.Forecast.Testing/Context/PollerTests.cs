e7e6cd2731c4da00d2a9b0c034751f853044fe7d
Steven Nagy <geiganax@gmail.com>
2018-06-15 14:44:53 -0400

    [#158038852] fixed params for Update methdos

--- src/Housing.Forecast.Testing/Context/PollerTests.cs
+++ src/Housing.Forecast.Testing/Context/PollerTests.cs
@@ -42,13 +42,30 @@ namespace Housing.Forecast.Testing.Context
             {
                 Poller testPoller = new Poller(_context, TimeSpan.MinValue);
                 Name insertTest = getNewName();
-                _output.WriteLine(insertTest.NameId.ToString());
                 testPoller.UpdateName(insertTest);
                 _context.SaveChanges();
                 Name afterInsertTest = _context.Names.Where(p => p.NameId == insertTest.NameId).FirstOrDefault();
                 Assert.Equal(insertTest, afterInsertTest);
             }
         }
+
+        [Fact]
+        public void UpdateModFirstName()
+        {
+            using (_context = new ForecastContext(options))
+            {
+                Poller testPoller = new Poller(_context, TimeSpan.MinValue);
+                Name insertTest = getNewName();
+                testPoller.UpdateName(insertTest);
+                _context.SaveChanges();
+                insertTest.First = "testChanged";
+                testPoller.UpdateName(insertTest);
+                _context.SaveChanges();
+                Name afterInsertTest = _context.Names.Where(p => p.NameId == insertTest.NameId).FirstOrDefault();
+                Assert.Equal(insertTest, afterInsertTest);
+            }
+        }
+
 
     }
 }
