f7d7d283422063c4481cbe4500b1e524438e4307
Steven Nagy <geiganax@gmail.com>
2018-06-15 14:49:24 -0400

    [#158038852] fixed interface

--- src/Housing.Forecast.Testing/Context/PollerTests.cs
+++ src/Housing.Forecast.Testing/Context/PollerTests.cs
@@ -66,6 +66,40 @@ namespace Housing.Forecast.Testing.Context
             }
         }
 
+        [Fact]
+        public void UpdateModMiddleName()
+        {
+            using (_context = new ForecastContext(options))
+            {
+                Poller testPoller = new Poller(_context, TimeSpan.MinValue);
+                Name insertTest = getNewName();
+                testPoller.UpdateName(insertTest);
+                _context.SaveChanges();
+                insertTest.Middle = "testChanged";
+                testPoller.UpdateName(insertTest);
+                _context.SaveChanges();
+                Name afterInsertTest = _context.Names.Where(p => p.NameId == insertTest.NameId).FirstOrDefault();
+                Assert.Equal(insertTest, afterInsertTest);
+            }
+        }
+
+        [Fact]
+        public void UpdateModLastName()
+        {
+            using (_context = new ForecastContext(options))
+            {
+                Poller testPoller = new Poller(_context, TimeSpan.MinValue);
+                Name insertTest = getNewName();
+                testPoller.UpdateName(insertTest);
+                _context.SaveChanges();
+                insertTest.Last = "testChanged";
+                testPoller.UpdateName(insertTest);
+                _context.SaveChanges();
+                Name afterInsertTest = _context.Names.Where(p => p.NameId == insertTest.NameId).FirstOrDefault();
+                Assert.Equal(insertTest, afterInsertTest);
+            }
+        }
+
 
     }
 }
