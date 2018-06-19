a5df5b775b17d49ae125f1d659773139d976789b
Kay Choi <kchoi@mail.sfsu.edu>
2018-06-18 17:38:19 -0700

    Removed async modifier from Poll() method. [#158038852]

--- src/Housing.Forecast.Context/Poller.cs
+++ src/Housing.Forecast.Context/Poller.cs
@@ -380,7 +380,7 @@ namespace Housing.Forecast.Context
         /// is requested. Exception for some logging.
         /// Potentially add retry pattern if db connection failed.
         /// </remarks>
-        public async void Poll()
+        public void Poll()
         {
             CancellationToken cancellation = cts.Token;
             TimeSpan interval = _interval;
@@ -388,7 +388,7 @@ namespace Housing.Forecast.Context
             {
                 try
                 {
-                    await Update();
+                    Update();
                     if (cancellation.IsCancellationRequested)
                     {
                         break;
